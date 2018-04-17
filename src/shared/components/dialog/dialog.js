5a9af34690e39e9e42c4a83d82b51dd8f574bb5a
tfpractice <jackson.brebnor@gmail.com>
2018-04-17 16:28:55 -0400

    adds dialog

--- src/shared/components/dialog/dialog.js
+++ src/shared/components/dialog/dialog.js
@@ -1,29 +1,19 @@
 import Button from "material-ui/Button";
-import Card, { CardActions, CardContent, CardHeader } from "material-ui/Card";
-import Dialog, { DialogActions, DialogTitle } from "material-ui/Dialog";
+import Card, { CardActions, CardContent } from "material-ui/Card";
+import Dialog, { DialogTitle } from "material-ui/Dialog";
 import Grid from "material-ui/Grid";
 import React from "react";
 import Text from "material-ui/Typography";
 import { connect } from "react-redux";
 
-import { withSwitch } from "../wrappers";
-
 const SubmissionDialog = ({ open, stop, formVals, ...props }) => (
   <Dialog open={open}>
-    <Grid
-      container
-      justify="center"
-      alignContent="center"
-      alignItems="center"
-    >
+    <Grid container justify="center" alignContent="center" alignItems="center">
       <Grid item xs={11}>
         {` `}
         <DialogTitle>{`Thanks for Submitting your a Form <3 ${
           formVals.firstName
         } ${formVals.lastName} `}</DialogTitle>
-        <CardActions>
-          <Button onClick={stop}>close</Button>
-        </CardActions>
       </Grid>
       <Grid item xs={11}>
         <Card>
@@ -35,12 +25,15 @@ const SubmissionDialog = ({ open, stop, formVals, ...props }) => (
               alignItems="center"
             >
               <Grid item xs={11}>
-                <Text>{formVals.lastName} </Text>
-                <Text>{formVals.age} </Text>
+                <Text>email:{formVals.email} </Text>
+                <Text>age: {formVals.age} </Text>
                 <Text>
-                  {formVals.height.ft} {formVals.height.in}
+                  Height: {formVals.height.ft}' {formVals.height.in}"
                 </Text>
-                <Text>{formVals.color} </Text>
+                <Text>
+                  favorite color:{formVals.color_other || formVals.color}
+                  {` `}
+                </Text>
               </Grid>
             </Grid>
           </CardContent>
